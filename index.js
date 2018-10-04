5efd451dda64c4736424ca7b5e70f18ef444915d
bipinm <bips3421@yahoo.com>
2018-10-04 01:57:11 -0700

    2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hi There.. how are you ?')
 });
  
-console.log('Application listening on port 8080');
+console.log('Application listening on port 8080...');
 app.listen(8080);
