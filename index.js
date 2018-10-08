fdf2644c3c036638808a674bf822bdd3940c381e
bipinm <bips3421@yahoo.com>
2018-10-07 22:41:56 -0700

    3

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const basicAuth = require('express-basic-auth');
+const bodyParser = require('body-parser');
  
+app.use(basicAuth({
+    users: { 'admin': 'secret' }
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.send('Hi There.. how are you ?')
 });
  
+
+
+app.post('/getMovieList',function (request,response)  {
+
+  console.log(request.body);
+
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({
+    "speech" : "Error. Can you try it again ? ",
+    "displayText" : "Error. Can you try it again ? "
+  }));
+});
+
+
 console.log('Application listening on port 8080...');
 app.listen(8080);
